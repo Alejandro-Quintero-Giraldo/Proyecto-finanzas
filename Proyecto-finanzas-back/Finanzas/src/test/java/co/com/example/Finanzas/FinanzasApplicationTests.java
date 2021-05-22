7e9cce2684dfca8e8f3ce48ebfbf910c1504ff32
Alejandro Quintero <alejo08h.aqg@gmail.com>
2021-05-22 14:37:25 -0500

    primer commit

---
+++ Proyecto-finanzas-back/Finanzas/src/test/java/co/com/example/Finanzas/FinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.example.Finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
